30b42b7442e804d745030bc43fa31afcd06555c3
shaheenj <jawadishaheen@gmail.com>
2025-03-01 02:58:39 +0100

    Implement user authentication in product routes and enhance auth utility functions

--- backend/routers/product.py
+++ backend/routers/product.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, Depends, status
 from typing import Dict
 from sqlalchemy.orm import Session
+
+from models.user import User
 from services import product as productService
 from database import get_db
 from schemas.product import Product, ProductCreate, ProductUpdate
+from utilities import auth
 
 router = APIRouter(
     prefix="/products",
-    tags=["products"],
+    tags=["products"]
 )
 
 
@@ -22,10 +25,12 @@ async def get_product(
     )
 
 
-@router.post("/", response_model=Product, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=Product,
+             status_code=status.HTTP_201_CREATED)
 async def create_product(
         product: ProductCreate,
         db: Session = Depends(get_db),
+        current_user: User = Depends(auth.get_current_user)
 ):
     return productService.create_product(
         db=db,
@@ -38,6 +43,7 @@ async def update_product(
         product_id: int,
         product: ProductUpdate,
         db: Session = Depends(get_db),
+        current_user: User = Depends(auth.get_current_user)
 ):
     return productService.update_product(
         db=db,
@@ -50,5 +56,6 @@ async def update_product(
 async def delete_product(
         product_id: int,
         db: Session = Depends(get_db),
+        current_user: User = Depends(auth.get_current_user)
 ):
     return productService.delete_product(db=db, product_id=product_id)
--- backend/utilities/auth.py
+++ backend/utilities/auth.py
@@ -1,16 +1,17 @@
 from datetime import datetime, timedelta
-
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
 from config import settings
+from database import get_db
 from models.user import User
+from schemas.user import TokenData
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{settings.API_PREFIX}/auth/login")
-
 
 
 def get_password_hash(password):
@@ -35,3 +36,25 @@ def create_access_token(data: dict):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+
+    user = db.query(User).filter(User.username == token_data.username).first()
+    if user is None:
+        raise credentials_exception
+    return user
