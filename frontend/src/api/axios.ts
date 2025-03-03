21ec4a3fc87e53c47dc51c370a6b4f08e49c4b40
shaheenj <jawadishaheen@gmail.com>
2025-03-03 08:10:20 +0100

    Add Axios instance with request/response interceptors for API calls

---
+++ frontend/src/api/axios.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { getToken, removeToken } from '../utils/token';
+
+const API_BASE_URL = 'http://localhost:8000/api';
+
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      removeToken();
+      window.location.href = '/login';
+    }
+    
+    const errorMessage =
+      error.message ||
+      'Something wrong';
+    
+    return Promise.reject(new Error(errorMessage));
+  }
+);
+
+export default api;
