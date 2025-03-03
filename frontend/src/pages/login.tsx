41583d8c4ea6cda42540d1d0966ba43c6d8bb2d1
shaheenj <jawadishaheen@gmail.com>
2025-03-03 23:15:45 +0100

    Refactor login page layout and styles: enhance UI with new design and integrate global styles

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,39 +1,55 @@
-import React from 'react';
-import { useAuthForm } from '../hooks/forms/useAuthForm';
+import React from "react";
+import { useAuthForm } from "../hooks/forms/useAuthForm";
 
 const Login: React.FC = () => {
   const { formik, isLoading } = useAuthForm();
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.errors.email && formik.touched.email && <p>{formik.errors.email}</p>}
-      </div>
+    <div className="auth_page">
+      <div className="auth_box">
+        <div className="auth_box_logo">
+          <img src="/logo.png" alt="" />
+        </div>
+        <div className="auth_box_title">
+        <h2>Login</h2>
+        </div>
+        <div className="form_holder">
+          <form onSubmit={formik.handleSubmit}>
+            <div>
+              <label>Email:</label>
+              <input
+                type="email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.errors.email && formik.touched.email && (
+                <p>{formik.errors.email}</p>
+              )}
+            </div>
 
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        {formik.errors.password && formik.touched.password && <p>{formik.errors.password}</p>}
-      </div>
+            <div>
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.errors.password && formik.touched.password && (
+                <p>{formik.errors.password}</p>
+              )}
+            </div>
 
-      <button type="submit" disabled={isLoading}>
-        {isLoading ? 'Logging In...' : 'Login'}
-      </button>
-    </form>
+            <button type="submit" disabled={isLoading}>
+              {isLoading ? "Logging In..." : "Login"}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
