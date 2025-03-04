fe19cd7eebced4a01699ad03985a5bbdb7f443a8
shaheenj <jawadishaheen@gmail.com>
2025-03-04 14:43:07 +0100

    Enhance Home page: add order by section and product view options

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,4 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faGrip } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import InputField from "../components/elements/input";
 
 const Home: React.FC = () => {
   return (
@@ -7,7 +10,7 @@ const Home: React.FC = () => {
         <div className="row">
           <div className="col-3 left_side">
             <div className="categories_box">
-              <div className="tit"> 
+              <div className="tit">
                 <h2>Categories</h2>
               </div>
 
@@ -18,17 +21,40 @@ const Home: React.FC = () => {
                 <li>cat 4</li>
               </ul>
             </div>
+
+            <div className="categories_box">
+              <div className="tit">
+                <h2>Order By</h2>
+              </div>
+
+              <ul>
+                <li> 1</li>
+                <li> 2</li>
+              </ul>
+            </div>
           </div>
 
           <div className="col-9 right_side">
-            <div className="top_section">prod by page | view | search</div>
-            <div className="products_list">
-              <div className="row">
-                <div className="col-4">prod</div>
-                <div className="col-4">prod</div>
-                <div className="col-4">prod</div>
-                <div className="col-4">prod</div>
+            <div className="top_section">
+              <div className="view_style">
+                <div className="single_view selected">
+                  <FontAwesomeIcon icon={faGrip} />
+                </div>
+                <div className="single_view">
+                  <FontAwesomeIcon icon={faBars} />
+                </div>
               </div>
+
+              <div className="search_section"> </div>
+              <div className="prod_by_page">prod by page</div>
+            </div>
+          </div>
+          <div className="products_list">
+            <div className="row">
+              <div className="col-4">prod</div>
+              <div className="col-4">prod</div>
+              <div className="col-4">prod</div>
+              <div className="col-4">prod</div>
             </div>
           </div>
         </div>
