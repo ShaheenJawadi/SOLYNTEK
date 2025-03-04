6cfbbe12383702b3f4653963f405f36e536c4672
shaheenj <jawadishaheen@gmail.com>
2025-03-04 14:14:18 +0100

    Enhance Home page layout: add categories section and improve product display

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -5,11 +5,31 @@ const Home: React.FC = () => {
     <div className="filter_page_holder">
       <div className="container">
         <div className="row">
-          <div className="col-3 left_side"></div>
+          <div className="col-3 left_side">
+            <div className="categories_box">
+              <div className="tit"> 
+                <h2>Categories</h2>
+              </div>
+
+              <ul>
+                <li>cat 1</li>
+                <li>cat 2</li>
+                <li>cat 3</li>
+                <li>cat 4</li>
+              </ul>
+            </div>
+          </div>
 
           <div className="col-9 right_side">
-            <div className="top_section"></div>
-            <div className="products_list"></div>
+            <div className="top_section">prod by page | view | search</div>
+            <div className="products_list">
+              <div className="row">
+                <div className="col-4">prod</div>
+                <div className="col-4">prod</div>
+                <div className="col-4">prod</div>
+                <div className="col-4">prod</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
