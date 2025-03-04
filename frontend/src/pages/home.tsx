b8b079b966354f71a047a9c992b356f7382a0aeb
shaheenj <jawadishaheen@gmail.com>
2025-03-04 15:37:01 +0100

    Enhance Home page: add product per page selection

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faGrip, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faGrip,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import InputField from "../components/elements/input";
+import SelectField from "../components/elements/select";
 
 const Home: React.FC = () => {
   return (
@@ -46,20 +51,26 @@ const Home: React.FC = () => {
               </div>
 
               <div className="search_section form_holder">
-
                 <InputField placeholder="Search..." />
                 <div className="search_btn">
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
                 </div>
-                
-                
-                
-
-                 </div>
-              <div className="prod_by_page">prod by page</div>
+              </div>
+              <div className="prod_by_page form_holder">
+                <div>
+                  <SelectField
+                    options={[
+                      { value: "10", label: "10" },
+                      { value: "20", label: "20" },
+                      { value: "50", label: "50" },
+                      { value: "100", label: "100" },
+                    ]}
+                  />
+                </div>
+                <div>Per page</div>
+              </div>
             </div>
-          </div>
-          <div className="products_list">
+            <div className="products_list">
             <div className="row">
               <div className="col-4">prod</div>
               <div className="col-4">prod</div>
@@ -67,6 +78,8 @@ const Home: React.FC = () => {
               <div className="col-4">prod</div>
             </div>
           </div>
+          </div>
+      
         </div>
       </div>
     </div>
